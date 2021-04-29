d27a2447198b43a0b8cda768eca593791f27d097
Elric Milton <elric.milton@yahoo.com.au>
2021-04-29 15:23:45 +1000

    Added wall spell power reduction & cooldown

--- Out Cast/Assets/Scripts/Magic/WallSpell.cs
+++ Out Cast/Assets/Scripts/Magic/WallSpell.cs
@@ -15,6 +15,9 @@ public class WallSpell : MonoBehaviour
     [SerializeField] float maxRange;
     Vector3 spawnPos;
     bool spawnable = false;
+    [SerializeField] GameEvent wallSpellEvent;
+    [SerializeField] float wallSpellCooldown;
+    bool waitingForCooldown = false;
 
     private void Awake()
     {
@@ -23,45 +26,53 @@ public class WallSpell : MonoBehaviour
 
     private void Update()
     {
-        RaycastOnClick();
+        if (Input.GetMouseButton(0) && !waitingForCooldown)
+        {           
+            RaycastOnMouseDown();
+        }
 
         if (Input.GetMouseButtonUp(0) && spawnable)
         {
-            wallSpawnMarker.SetActive(false);
-            wall.SetActive(true);
-            wall.transform.position = spawnPos;
-            spawnable = false;
+            PlaceWall();
         }
     }
 
-    private void RaycastOnClick()
+    private void RaycastOnMouseDown()
     {
-        if (Input.GetMouseButton(0))
-        {           
-            //Ray ray = cam.ScreenPointToRay(new Vector3(cam.scaledPixelWidth / 2, cam.scaledPixelHeight / 2));
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        //Ray ray = cam.ScreenPointToRay(new Vector3(cam.scaledPixelWidth / 2, cam.scaledPixelHeight / 2));
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out RaycastHit hit, maxRange+10, layerMask))
+        if (Physics.Raycast(ray, out RaycastHit hit, maxRange + 10, layerMask))
+        {
+            if (hit.distance <= maxRange)
             {
-                if (hit.distance <= maxRange)
-                {
-                    spawnable = true;
-                    wallSpawnMarker.SetActive(true);
-                    wallSpawnMarker.transform.position = new Vector3(hit.point.x, hit.point.y + markerOffset, hit.point.z);
-                    spawnPos = new Vector3(hit.point.x, hit.point.y + wallOffset, hit.point.z);
-                }
-                else if (hit.distance > maxRange)
-                {
-                    wallSpawnMarker.SetActive(false);
-                    spawnable = false;
-                }
+                spawnable = true;
+                wallSpawnMarker.SetActive(true);
+                wallSpawnMarker.transform.position = new Vector3(hit.point.x, hit.point.y + markerOffset, hit.point.z);
+                spawnPos = new Vector3(hit.point.x, hit.point.y + wallOffset, hit.point.z);
+            }
+            else if (hit.distance > maxRange)
+            {
+                wallSpawnMarker.SetActive(false);
+                spawnable = false;
             }
         }
     }
 
-
     void PlaceWall()
     {
+        wallSpawnMarker.SetActive(false);
+        wall.SetActive(true);
         wall.transform.position = spawnPos;
+        spawnable = false;
+        wallSpellEvent.Invoke();
+        StartCoroutine(Cooldown());
+    }
+
+    IEnumerator Cooldown()
+    {
+        waitingForCooldown = true;
+        yield return new WaitForSeconds(wallSpellCooldown);
+        waitingForCooldown = false;
     }
 }
