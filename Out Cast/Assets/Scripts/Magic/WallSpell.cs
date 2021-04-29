0af7abb311dc92580e9b3e7aa7ab6ee81ab6f6f4
Elric Milton <elric.milton@yahoo.com.au>
2021-04-29 10:48:48 +1000

    Removed unwanted event listener

---
+++ Out Cast/Assets/Scripts/Magic/WallSpell.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpell : MonoBehaviour
+{
+    [SerializeField] GameObject wallSpawnMarker;
+    [SerializeField] float wallSpawnRange;
+    Camera cam;
+    [SerializeField] LayerMask groundLayer;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            DoRaycast();
+        }
+    }
+    private void OnMouseDown()
+    {
+        Ray ray = cam.ScreenPointToRay(new Vector3(cam.scaledPixelWidth / 2, cam.scaledPixelHeight / 2, 0));
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, groundLayer))
+        {
+            if (hitInfo.distance <= wallSpawnRange)
+            {
+                wallSpawnMarker.SetActive(true);
+                wallSpawnMarker.transform.position = hitInfo.point;
+            }
+            else if (hitInfo.distance > wallSpawnRange)
+            {
+                wallSpawnMarker.SetActive(false);
+            }
+        }
+    }
+    void DoRaycast()
+    {
+
+    }
+}
