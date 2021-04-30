f6495317899f89fab963a221b25da4c9685f4cd3
Caerwyn <c_barts@live.com>
2021-04-30 14:41:54 +1000

    Pedestrian walking on Navmesh

---
+++ Out Cast/Assets/Scripts/PedestrianSystem/PedestrianWander.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Unity.Collections;
+
+public class PedestrianWander : MonoBehaviour
+{
+    public float wanderRadius;
+    [Space(10)]
+    public float wanderTimer;
+    public float wanderTimerMax;
+    public float wanderTimerMin;
+    [Space(10)]
+    public float speed;
+    [Space(10)]
+    public float speedMin;
+    public float speedMax;
+
+    private NavMeshAgent agent;
+    public float timer;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        wanderTimer = Random.Range(wanderTimerMin, wanderTimerMax);
+        speed = Random.Range(speedMin, speedMax);
+        agent.speed = speed;
+        timer = 0;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            WanderRandomize();
+            timer = wanderTimer;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+
+    void WanderRandomize()
+    {
+        wanderTimer = Random.Range(wanderTimerMin, wanderTimerMax);
+    }
+}
