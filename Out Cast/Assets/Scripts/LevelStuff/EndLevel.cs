ddc6d7439e2eaa9fd7c1aad0e7bdd919cbfdd96d
Elric Milton <elric.milton@yahoo.com.au>
2021-05-07 16:38:48 +1000

    Added start and finish triggers

---
+++ Out Cast/Assets/Scripts/LevelStuff/EndLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    [SerializeField] GameEvent endLevelEvent;
+    GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            End();
+        }
+    }
+
+    void End()
+    {
+        endLevelEvent.Invoke();
+
+    }
+}
