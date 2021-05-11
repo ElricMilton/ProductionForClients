d1c5d4e6e9b91dcecd06b3f271b33570578751ed
Elric Milton <elric.milton@yahoo.com.au>
2021-05-12 09:45:49 +1000

    Added HidableArea prefab

---
+++ Out Cast/Assets/Scripts/HideableArea.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideableArea : MonoBehaviour
+{
+    [SerializeField] BoolVariable isChasable;
+    [SerializeField] BoolVariable isDischarging;
+    GameObject player;
+
+    private void OnEnable()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            isChasable.Value = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            isChasable.Value = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            isChasable.Value = false;
+            isDischarging.Value = false;
+        }
+    }
+}
