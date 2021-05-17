fba60f41960a67006403e1c0d4b1c8fbf92364b7
ElricM <elric.milton@yahoo.com.au>
2021-05-17 17:17:55 +1000

    Added wall sfx to end of lvl

--- Out Cast/Assets/Scripts/LevelStuff/EndLevel.cs
+++ Out Cast/Assets/Scripts/LevelStuff/EndLevel.cs
@@ -7,6 +7,7 @@ public class EndLevel : MonoBehaviour
     [SerializeField] GameEvent endLevelEvent;
     GameObject player;
     [SerializeField] Animator endWallAnim;
+    [SerializeField] AudioSource stoneSlideSFX;
 
     private void Awake()
     {
@@ -16,14 +17,16 @@ public class EndLevel : MonoBehaviour
     {
         if (other.gameObject == player)
         {
-            endWallAnim.Play("SafeZoneReveal");
+            
             StartCoroutine(End());
         }
     }
 
     IEnumerator End()
     {
-
+        stoneSlideSFX.Play();
+        yield return new WaitForSeconds(0.2f);
+        endWallAnim.Play("SafeZoneReveal");
         yield return new WaitForSeconds(3);
         endLevelEvent.Invoke();
 
