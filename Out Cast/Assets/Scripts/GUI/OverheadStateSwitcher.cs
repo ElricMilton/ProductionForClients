7ce8f5c62be96a3e04749e209b7f35101ea0387b
ElricM <elric.milton@yahoo.com.au>
2021-05-19 17:35:07 +1000

    Added state OverheadVisualiser images

---
+++ Out Cast/Assets/Scripts/GUI/OverheadStateSwitcher.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverheadStateSwitcher : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] GameObject alertImage;
+    [SerializeField] GameObject cowerImage;
+    [SerializeField] GameObject searchingImage;
+    [SerializeField] GameObject chasingImage;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public void OverheadAlertState()
+    {
+        alertImage.SetActive(true);
+        cowerImage.SetActive(false);
+        searchingImage.SetActive(false);
+        chasingImage.SetActive(false);
+    }
+    public void OverheadCowerState()
+    {
+        cowerImage.SetActive(true);
+        alertImage.SetActive(false);
+        searchingImage.SetActive(false);
+        chasingImage.SetActive(false);
+    }
+    public void OverheadSearchingState()
+    {
+        searchingImage.SetActive(true);
+        alertImage.SetActive(false);
+        cowerImage.SetActive(false);
+        chasingImage.SetActive(false);
+    }
+    public void OverheadChasingState()
+    {
+        chasingImage.SetActive(true);
+        alertImage.SetActive(false);
+        cowerImage.SetActive(false);
+        searchingImage.SetActive(false);
+    }
+    public void HideStateOverheads()
+    {
+        alertImage.SetActive(false);
+        cowerImage.SetActive(false);
+        searchingImage.SetActive(false);
+        chasingImage.SetActive(false);
+    }
+}
