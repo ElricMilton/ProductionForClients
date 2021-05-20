2ab87ee67c1153bd5f10d9d5b10bc002ad718a1e
Barty <c_barts@live.com>
2021-05-21 00:09:21 +1000

    Finished open sequence for main menu

--- Out Cast/Assets/Scripts/O'NeilBS/ChangePedestrianStatus.cs
+++ Out Cast/Assets/Scripts/O'NeilBS/ChangePedestrianStatus.cs
@@ -1,41 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class ChangePedestrianStatus : MonoBehaviour
-{
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using SensorToolkit;
+//public class ChangePedestrianStatus : MonoBehaviour
+//{
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using SensorToolkit;
 
-public class SetPedestrianState : MonoBehaviour
-{
-    public RangeSensor rangeSensor;
+//public class SetPedestrianState : MonoBehaviour
+//{
+//    public RangeSensor rangeSensor;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+//    // Start is called before the first frame update
+//    void Start()
+//    {
 
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
 
-    }
+//    }
 
-    public void SetPedestrianCower()
-    {
-        var allPedestrian = rangeSensor.GetDetected();
-        foreach (GameObject ped in allPedestrian)
-        {
-            ped.GetComponent<PedestrianBehaviour>().CowerCheck();
-        }
+//    public void SetPedestrianCower()
+//    {
+//        var allPedestrian = rangeSensor.GetDetected();
+//        foreach (GameObject ped in allPedestrian)
+//        {
+//            ped.GetComponent<PedestrianBehaviour>().CowerCheck();
+//        }
 
-        var peddestrian = rangeSensor.GetNearest();
-        peddestrian.GetComponent<PedestrianBehaviour>().GetCopCheck();
-    }
+//        var peddestrian = rangeSensor.GetNearest();
+//        peddestrian.GetComponent<PedestrianBehaviour>().GetCopCheck();
+//    }
 
-}
-}
+//}
+//}
