1223ce73732c72da7ebd529b88d26bc0bc9c5da3
Elric Milton <elric.milton@yahoo.com.au>
2021-05-20 12:03:59 +1000

    Added Input Prompt prefab and functionality

---
+++ Out Cast/Assets/Scripts/GUI/InputPrompts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPrompts : MonoBehaviour
+{
+    [SerializeField] GameObject popup;
+    [SerializeField] Animator anim;
+    GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            popup.SetActive(true);
+            anim.Play("DialogueSpawn");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            anim.Play("DialogueDespawn");
+        }
+    }
+}
