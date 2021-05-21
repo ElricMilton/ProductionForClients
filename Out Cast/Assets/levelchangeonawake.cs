ed0cfd2f5d44ea5d7cfca4647a84cf5a5ebb934e
Caerwyn <c_barts@live.com>
2021-05-21 12:12:43 +1000

    Main Menu scene load on end of intro sequence

---
+++ Out Cast/Assets/levelchangeonawake.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelchangeonawake : MonoBehaviour
+{
+    public string level;
+
+    void Start()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
